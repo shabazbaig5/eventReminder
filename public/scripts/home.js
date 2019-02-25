08368b9bbf5a8c13f818bb60657592331fca570a
Ethan Hunt <shabazbaig5@gmail.com>
2019-02-25 20:58:40 +0530

    addingEvents

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,1 +1,94 @@
+
 console.log('script loaded');
+
+// deleteEvent = (e) => {
+//     console.log('clicked');
+//     console.log(e);
+//     console.loh(e.target.value);
+//     // fetch('/deleteEvent')
+//     //     .then(() => {
+
+//     //     })
+// }
+
+
+var eventList = document.querySelectorAll('.centerPaper p');
+
+
+eventList.forEach((list) => {
+    list.addEventListener('click',(e) => {
+        let str = e.target.innerHTML;
+        console.log(e.target.innerHTML);
+        for(let i= 0; i<str.length; i++){
+            if(str[i]===':'){
+                str = str.slice(0,i-1);
+                break;
+            }
+        }
+    
+        fetch(`/deleteEvent/${str}`)
+            .then((result) => {
+                
+                return result;
+            }).then((result) => {
+               location.reload();
+            })
+        console.log(str);
+        console.log(str.length);
+    });
+});
+
+
+
+console.log(eventList);
+
+
+
+// let centerPaper = document.querySelector('.centerPaper');
+
+// let para = centerPaper.querySelector('p');
+
+
+
+
+// eventList.addEventListener('click', (e) => {
+//     let str = e.target.innerHTML;
+//     console.log(e.target.innerHTML);
+//     for(let i= 0; i<str.length; i++){
+//         if(str[i]===':'){
+//             str = str.slice(0,i-1);
+//             break;
+//         }
+//     }
+
+//     fetch(`/deleteEvent/${str}`)
+//         .then((result) => {
+            
+//             return result;
+//         }).then((result) => {
+//            location.reload();
+//         })
+//     console.log(str);
+//     console.log(str.length);
+// });
+
+// event.onclick = (e) => {
+//     let str = e.target.innerHTML;
+//     console.log(e.target.innerHTML);
+//     for(let i= 0; i<str.length; i++){
+//         if(str[i]===':'){
+//             str = str.slice(0,i-1);
+//             break;
+//         }
+//     }
+
+//     fetch(`/deleteEvent/${str}`)
+//         .then((result) => {
+//             // r    
+//             return result;
+//         }).then((result) => {
+//             console.log(result);
+//         })
+//     console.log(str);
+//     console.log(str.length);
+// }
--- server/routes/router.js
+++ server/routes/router.js
@@ -7,27 +7,64 @@ const router = express.Router({
 
 
 module.exports = () => {
-
+    //route to render home page
     router.get('/', (req,res) => {
         const db = require('../database/db');
 
         db.query('SELECT * FROM events', (err,rows,columns) => {
             if(err){
-                console.log(err);
+                // console.log(err);
             }
             else{
-                console.log(rows);
+                // console.log(rows);
                 res.render('home', {
                     events : rows
                 });
 
             }
         })
-    })
+    });
+
+
+    //post request to save the events
+    router.post('/', (req,res) => {
+        
+        const db = require('../database/db');
+        console.log(req.body);
+
+        db.query("INSERT INTO events values(NULL, '"+req.body.name+"','"+req.body.event+"','"+req.body.date+"')", (err,rows,columns) => {
+            if(err){
+                console.log(err);
+            }
+            else{
+                // res.render("home", {
+                //     mesg : "event added!"
+                // });
+                res.redirect('/');
+            }
+        });
+
+    });
+
+
+    //deleting the user API
+    router.get('/deleteEvent/:id', (req,res) => {
+        const db = require('../database/db');
+
+        db.query("DELETE from events where(name='"+req.params.id+"')", (err,rows,colomns) => {
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.send('success');
+            }
+        })
+    });
 
     router.get('/home',(req,res) => {
         res.render('events');
     });
+
 
 
 
