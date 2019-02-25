8983d6849ac0dd2c45c65a3cdf8d9856d32470c4
Ethan Hunt <shabazbaig5@gmail.com>
2019-02-25 12:14:41 +0530

    '3rdcommit'

--- server/index.js
+++ server/index.js
@@ -24,6 +24,23 @@ app.use('/', homeRoute());
 
 
 
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   // TODO: Log the answer in a database
+//   console.log(`Thank you for your valuable feedback: ${answer}`);
+
+//   rl.close();
+// });
+
+// rl.question('What do you do?', (answer) => {
+//     console.log('Thankssssssssss');
+// });
 
 app.listen(3000, () => {
     console.log('listening to port 3000');
